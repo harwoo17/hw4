7c6f3c521793699bebc5e8f3c2e57625c13c0fd3
harwoo17 <Joe.Harwood@kellogg.northwestern.edu>
2026-03-18 18:06:29 +0000

    Sixth Fix

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,27 +1,21 @@
-class EntriesController < ApplicationController
-  def new
-    if session["user_id"] == nil
-      redirect_to "/login"
-      return
-    end
-
-    @place_id = params["place_id"]
+def create
+  if session["user_id"] == nil
+    redirect_to "/login"
+    return
   end
 
-  def create
-    if session["user_id"] == nil
-      redirect_to "/login"
-      return
-    end
+  @entry = Entry.new
+  @entry["title"] = params["title"]
+  @entry["description"] = params["description"]
+  @entry["occurred_on"] = params["occurred_on"]
+  @entry["place_id"] = params["place_id"]
+  @entry["user_id"] = session["user_id"]
 
-    @entry = Entry.new
-    @entry["title"] = params["title"]
-    @entry["description"] = params["description"]
-    @entry["occurred_on"] = params["occurred_on"]
-    @entry["place_id"] = params["place_id"]
-    @entry["user_id"] = session["user_id"]
-
-    @entry.save
-    redirect_to "/places/#{@entry["place_id"]}"
+  if params["image"]
+    @entry.image.attach(params["image"])
   end
+
+  @entry.save
+
+  redirect_to "/places/#{@entry["place_id"]}"
 end
