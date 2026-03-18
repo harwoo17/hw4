e0891560848f7c0b2509999899c338df3838f2d5
harwoo17 <Joe.Harwood@kellogg.northwestern.edu>
2026-03-18 18:15:17 +0000

    Ninth Fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,13 +5,5 @@ Rails.application.configure do
   config.eager_load = false
   config.consider_all_requests_local = true
   config.server_timing = true
-
-  config.action_controller.perform_caching = false
-
   config.active_storage.service = :local
-
-  config.active_support.deprecation = :log
-  config.active_record.migration_error = :page_load
-  config.active_record.verbose_query_logs = true
-  config.assets.quiet = true
 end
